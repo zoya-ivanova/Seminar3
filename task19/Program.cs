c60512e8efd46b3fb593693c6a3bfd8b91dcd549
geekbrains <info@geekbrains.ru>
2022-11-20 13:53:00 +0500

    tas19

---
+++ task19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int division1 = number / 10000;
+int remainder1 = number % 10;
+if(division1 == remainder1)
+    {number = number / 10;
+    int division2 = (number / 100) % 10;  
+    int remainder2 = number % 10;
+    if(division2 == remainder2)
+                    Console.WriteLine("Да");
+    }
+else 
+Console.WriteLine("Нет");
