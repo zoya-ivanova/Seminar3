9731d7716279f42122426af6a1651478a50cdade
geekbrains <info@geekbrains.ru>
2022-11-20 13:57:52 +0500

    tas19-1

--- task19/Program.cs
+++ task19/Program.cs
@@ -10,12 +10,16 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int division1 = number / 10000;
 int remainder1 = number % 10;
+
 if(division1 == remainder1)
-    {number = number / 10;
+    
+    {
+    number = number / 10;
     int division2 = (number / 100) % 10;  
     int remainder2 = number % 10;
     if(division2 == remainder2)
-                    Console.WriteLine("Да");
+    Console.WriteLine("Да");
     }
+    
 else 
 Console.WriteLine("Нет");
